067a3cea978243c1ec3f89286fcdd8afb6dd66b3
aticcmahbub <aticcmahbub@gmail.com>
2025-08-03 02:15:17 +0600

    feat: add modal for task input

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,3 +1,4 @@
+import {AddTaskModal} from '@/components/module/tasks/AddTaskModal';
 import TaskCard from '@/components/module/tasks/TasksCard';
 import {selectTasks} from '@/redux/features/task/taskSlice';
 import {useAppSelector} from '@/redux/hook';
@@ -7,8 +8,9 @@ export default function Tasks() {
     console.log(tasks);
     return (
         <div className='mx-auto max-w-7xl px-5 mt-20'>
-            <div>
+            <div className='flex justify-between items-center'>
                 <h1>Tasks</h1>
+                <AddTaskModal />
             </div>
             <div className='space-y-5 mt-5'>
                 {tasks.map((task) => (
