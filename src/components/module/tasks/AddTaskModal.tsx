43005378bafb4d4290fa7951609584fea7bfc324
aticcmahbub <aticcmahbub@gmail.com>
2025-08-03 11:14:22 +0600

    fix:ewsolve warnings and errors:

--- src/components/module/tasks/AddTaskModal.tsx
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -3,6 +3,7 @@ import {
     Dialog,
     DialogClose,
     DialogContent,
+    DialogDescription,
     DialogFooter,
     DialogHeader,
     DialogTitle,
@@ -16,6 +17,7 @@ import {
     FormLabel,
 } from '@/components/ui/form';
 import {Input} from '@/components/ui/input';
+import {Textarea} from '@/components/ui/textarea';
 import {useForm} from 'react-hook-form';
 
 export function AddTaskModal() {
@@ -26,38 +28,62 @@ export function AddTaskModal() {
     };
     return (
         <Dialog>
-            <form>
-                <DialogTrigger asChild>
-                    <Button variant='outline'>Add Task</Button>
-                </DialogTrigger>
-                <DialogContent className='sm:max-w-[425px]'>
-                    <DialogHeader>
-                        <DialogTitle>Add Task</DialogTitle>
-                    </DialogHeader>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FormField
-                                control={form.control}
-                                name='title'
-                                render={({field}) => (
-                                    <FormItem>
-                                        <FormLabel />
-                                        <FormControl>
-                                            <Input {...field} />
-                                        </FormControl>
-                                    </FormItem>
-                                )}
-                            />
-                            <DialogFooter>
-                                <DialogClose asChild>
-                                    <Button variant='outline'>Cancel</Button>
-                                </DialogClose>
-                                <Button type='submit'>Save changes</Button>
-                            </DialogFooter>
-                        </form>
-                    </Form>
-                </DialogContent>
-            </form>
+            <DialogTrigger asChild>
+                <Button variant='outline'>Add Task</Button>
+            </DialogTrigger>
+
+            <DialogContent className='sm:max-w-[425px]'>
+                <DialogHeader>
+                    <DialogTitle>Add Task</DialogTitle>
+                    <DialogDescription className='sr-only'>
+                        Fill up this form for adding Task
+                    </DialogDescription>
+                </DialogHeader>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='space-y-4'
+                    >
+                        <FormField
+                            control={form.control}
+                            name='title'
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Title</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            value={field.value || ''}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='description'
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Description</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            {...field}
+                                            value={field.value || ''}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <DialogFooter>
+                            <DialogClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DialogClose>
+                            <Button type='submit'>Save changes</Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
         </Dialog>
     );
 }
